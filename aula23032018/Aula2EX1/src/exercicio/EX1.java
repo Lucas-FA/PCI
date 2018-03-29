74f9c90e8991123295fce0726c2d5f5721293433
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2018-03-29 14:20:35 -0300

    Aula 2 Exercício 1

---
+++ aula23032018/Aula2EX1/src/exercicio/EX1.java
@@ -0,0 +1,13 @@
+package exercicio;
+
+public class EX1 {
+	public static void main(String[] args) {
+		int num = 10;
+		
+		System.out.println("Números pares entre 11 e 250");
+		while (num<250) {
+		num += 2;
+		System.out.println(num);
+		}
+	}
+}
