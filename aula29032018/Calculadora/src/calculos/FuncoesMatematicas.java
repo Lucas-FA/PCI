d76e684b0d00d8522585fc48332a2995c0f12e35
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2018-04-12 15:12:50 -0300

    Calculadora

---
+++ aula29032018/Calculadora/src/calculos/FuncoesMatematicas.java
@@ -0,0 +1,64 @@
+package calculos;
+
+public class FuncoesMatematicas {
+	
+	public double mAdicao(double num0, double num1) {
+    	double resultado = num0 + num1;
+    	return resultado;
+    }
+	
+	public double mSubtracao(double num0, double num1) {
+    	double resultado = num0 - num1;
+    	return resultado;
+    }
+	
+	public double raizQuadrada(double num0) {
+    	double resultado = java.lang.Math.sqrt(num0);
+    	return resultado;
+    }
+	
+	public double divisao(double num0, double num1) {
+    	double resultado = num0 / num1;
+    	return resultado;
+    }
+	
+	public double divisaoPorcentagem(double num0, double num1) {
+    	double resultado = num0 / ((num0 * num1) / 100);
+    	return resultado;
+    }
+	
+	public double multiplicacaoPorcentagem(double num0, double num1) {
+    	double resultado = num0 * ((num0 * num1) / 100);
+    	return resultado;
+    }
+	
+	public double subtracaoPorcentagem(double num0, double num1) {
+    	double resultado = num0 - (num0 * num1) / 100;
+    	return resultado;
+    }
+	
+	public double adicaoPorcentagem(double num0, double num1) {
+    	double resultado = num0 + (num0 * num1) / 100;
+    	return resultado;
+    }
+	
+	public double porcentagem(double num0, double num1) {
+    	double resultado = num0 * num1 / 100;
+    	return resultado;
+    }
+	
+	public double multiplicacao(double num0, double num1) {
+    	double resultado = num0 * num1;
+    	return resultado;
+    }
+	
+    public double subtracao(double num0, double num1) {
+    	double resultado = num0 - num1;
+    	return resultado;
+    }
+    
+    public double adicao(double num0, double num1) {
+    	double resultado = num0 + num1;
+    	return resultado;
+    }
+}
--- aula29032018/Calculadora/src/view/Calculadora.java
+++ aula29032018/Calculadora/src/view/Calculadora.java
@@ -1,7 +1,11 @@
 package view;
 
+import calculos.FuncoesMatematicas;
+
 import java.awt.Container;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,7 +13,7 @@ import javax.swing.JTextField;
 
 public class Calculadora extends JFrame {
 	
-	JTextField txtVisor = new JTextField();
+	JTextField txtVisor = new JTextField("0");
 	
 	JButton btnMC = new JButton("MC");
 	JButton btnMR = new JButton("MR");
@@ -45,6 +49,11 @@ public class Calculadora extends JFrame {
 	JButton btnVirgula = new JButton(",");
 	JButton btnMais = new JButton("+");
 
+	FuncoesMatematicas mat = new FuncoesMatematicas();
+	String sinal = null, sinal0 = null;
+	double valor0 = 0, valor1 = 0, valorM = 0;
+	String str, str0;
+	
 	public Calculadora() {
 		super("Calculadora");
 		
@@ -54,66 +63,221 @@ public class Calculadora extends JFrame {
 		txtVisor.setBounds(10, 15, 171, 50);
 		paine.add(txtVisor);
 		
+		
 		btnMC.setBounds(10, 70, 30, 25);
 		btnMC.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMC);
+		btnMC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valorM = 0;
+			}
+		});
 		btnMR.setBounds(45, 70, 30, 25);
 		btnMR.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMR);
+		btnMR.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtVisor.setText(valorM + "");
+				if (sinal.equals("mmais")) {
+					valorM = Double.parseDouble(mat.mAdicao(valorM, valor0) + "");
+					txtVisor.setText(valorM + "");
+				}
+				else if(sinal.equals("mmenos")) {
+					valorM = Double.parseDouble(mat.mSubtracao(valorM, valor0) + "");
+					txtVisor.setText(valorM + "");
+				}	
+			}
+		});
 		btnMS.setBounds(80, 70, 30, 25);
 		btnMS.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMS);
+		btnMS.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valorM = Double.parseDouble(txtVisor.getText());
+			}
+		});
 		btnMmais.setBounds(115, 70, 30, 25);
 		btnMmais.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMmais);
+		btnMmais.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "mmais";
+			}
+		});
+		
 		btnMmenos.setBounds(150, 70, 30, 25);
 		btnMmenos.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMmenos);
+		btnMmenos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "mmenos";
+			}
+		});
 		
 		btnSetaEsquerda.setBounds(10, 100, 30, 25);
 		btnSetaEsquerda.setMargin(new Insets(1,1,1,1));
 		paine.add(btnSetaEsquerda);
+		btnSetaEsquerda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				str = txtVisor.getText();
+				str0 = str.substring(0, str.length() - 1);
+				txtVisor.setText(str0);
+			}
+		});
 		btnCE.setBounds(45, 100, 30, 25);
 		btnCE.setMargin(new Insets(1,1,1,1));
 		paine.add(btnCE);
+		btnCE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtVisor.setText("0");
+			}
+		});
 		btnC.setBounds(80, 100, 30, 25);
 		btnC.setMargin(new Insets(1,1,1,1));
 		paine.add(btnC);
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = 0;
+				txtVisor.setText("0");
+			}
+		});
 		btnMaisMenos.setBounds(115, 100, 30, 25);
 		btnMaisMenos.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMaisMenos);
+		btnMaisMenos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (sinal == "negativo") {
+					txtVisor.setText(txtVisor.getText().replace("-", " "));
+					sinal = "positivo";
+				}
+				else if (sinal == "positivo") {
+					txtVisor.setText(txtVisor.getText().replace(" ", "-"));
+					sinal = "negativo";
+				}
+				else
+					txtVisor.setText("-" + txtVisor.getText());
+				sinal = "negativo";
+			}
+		});
 		btnRaizQuadrada.setBounds(150, 100, 30, 25);
 		btnRaizQuadrada.setMargin(new Insets(1,1,1,1));
 		paine.add(btnRaizQuadrada);
+		btnRaizQuadrada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				txtVisor.setText(mat.raizQuadrada(valor0) + "");
+			}
+		});
 		
 		btnSete.setBounds(10, 130, 30, 25);
 		btnSete.setMargin(new Insets(1,1,1,1));
 		paine.add(btnSete);
+		btnSete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("7");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "7");
+				}
+			}
+		});
 		btnOito.setBounds(45, 130, 30, 25);
 		btnOito.setMargin(new Insets(1,1,1,1));
 		paine.add(btnOito);
+		btnOito.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("8");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "8");
+				}
+			}
+		});
 		btnNove.setBounds(80, 130, 30, 25);
 		btnNove.setMargin(new Insets(1,1,1,1));
 		paine.add(btnNove);
+		btnNove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("9");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "9");
+				}
+			}
+		});
 		btnDivisao.setBounds(115, 130, 30, 25);
 		btnDivisao.setMargin(new Insets(1,1,1,1));
 		paine.add(btnDivisao);
+		btnDivisao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "divisão";
+				txtVisor.setText("0");
+			}
+		});
 		btnPorcentagem.setBounds(150, 130, 30, 25);
 		btnPorcentagem.setMargin(new Insets(1,1,1,1));
 		paine.add(btnPorcentagem);
-		
+		btnPorcentagem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor1 = Double.parseDouble(txtVisor.getText());
+				sinal0 = "porcentagem";
+			}
+		});
 		btnQuatro.setBounds(10, 160, 30, 25);
 		btnQuatro.setMargin(new Insets(1,1,1,1));
 		paine.add(btnQuatro);
+		btnQuatro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("4");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "4");
+				}
+			}
+		});
 		btnCinco.setBounds(45, 160, 30, 25);
 		btnCinco.setMargin(new Insets(1,1,1,1));
 		paine.add(btnCinco);
+		btnCinco.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("5");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "5");
+				}
+			}
+		});
 		btnSeis.setBounds(80, 160, 30, 25);
 		btnSeis.setMargin(new Insets(1,1,1,1));
 		paine.add(btnSeis);
+		btnSeis.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("6");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "6");
+				}
+			}
+		});
 		btnMultiplicacao.setBounds(115, 160, 30, 25);
 		btnMultiplicacao.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMultiplicacao);
+		btnMultiplicacao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "multiplicação";
+				txtVisor.setText("0");
+			}
+		});
 		btn1barrax.setBounds(150, 160, 30, 25);
 		btn1barrax.setMargin(new Insets(1,1,1,1));
 		paine.add(btn1barrax);
@@ -121,28 +285,130 @@ public class Calculadora extends JFrame {
 		btnUm.setBounds(10, 190, 30, 25);
 		btnUm.setMargin(new Insets(1,1,1,1));
 		paine.add(btnUm);
+		btnUm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("1");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "1");
+				}
+			}
+		});
 		btnDois.setBounds(45, 190, 30, 25);
 		btnDois.setMargin(new Insets(1,1,1,1));
 		paine.add(btnDois);
+		btnDois.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("2");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "2");
+				}
+			}
+		});
 		btnTres.setBounds(80, 190, 30, 25);
 		btnTres.setMargin(new Insets(1,1,1,1));
 		paine.add(btnTres);
+		btnTres.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("3");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "3");
+				}
+			}
+		});
 		btnMenos.setBounds(115, 190, 30, 25);
 		btnMenos.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMenos);
+		btnMenos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "subtração";
+				txtVisor.setText("0");
+			}
+		});
 		btnIgual.setBounds(150, 190, 30, 55);
 		btnIgual.setMargin(new Insets(1,1,1,1));
 		paine.add(btnIgual);
+		btnIgual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor1 = Double.parseDouble(txtVisor.getText());
+				
+				if (sinal.equals("divisão")) {
+					txtVisor.setText(mat.divisao(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("multiplicação")) {
+					txtVisor.setText(mat.multiplicacao(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("subtração")) {
+					txtVisor.setText(mat.subtracao(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("adição")) {
+					txtVisor.setText(mat.adicao(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("divisão") && sinal0.equals("porcentagem")) {
+					txtVisor.setText(mat.porcentagem(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("multiplicação") && sinal0.equals("porcentagem")) {
+					txtVisor.setText(mat.porcentagem(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("subtração") && sinal0.equals("porcentagem")) {
+					txtVisor.setText(mat.porcentagem(valor0, valor1) + "");
+				}
+				
+				else if (sinal.equals("adição") && sinal0.equals("porcentagem")) {
+					txtVisor.setText(mat.porcentagem(valor0, valor1) + "");
+				}	
+			}
+		});
 		
 		btnZero.setBounds(10, 220, 65, 25);
 		btnZero.setMargin(new Insets(1,1,1,1));
 		paine.add(btnZero);
+		btnZero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(txtVisor.getText().equals("0")) {
+					txtVisor.setText("0");
+				}
+				else {
+					txtVisor.setText(txtVisor.getText() + "0");
+				}
+			}
+		});
 		btnVirgula.setBounds(80, 220, 30, 25);
 		btnVirgula.setMargin(new Insets(1,1,1,1));
 		paine.add(btnVirgula);
+		btnVirgula.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (sinal == "virgula") {
+					txtVisor.setText(txtVisor.getText());
+				}
+				else
+					txtVisor.setText(txtVisor.getText() + ".");
+				sinal = "virgula";
+			}
+		});
+		
 		btnMais.setBounds(115, 220, 30, 25);
 		btnMais.setMargin(new Insets(1,1,1,1));
 		paine.add(btnMais);
+		btnMais.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				valor0 = Double.parseDouble(txtVisor.getText());
+				sinal = "adição";
+				txtVisor.setText("0");
+			}
+		});
 		
 		this.setSize(197, 300);
 		this.setVisible(true);
